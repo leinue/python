812372665614c24b6a9b9ed57a6d4cc77115e905
ivy <597055914@qq.com>
2014-10-28 22:42:59 +0800

    hello world

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+Python 3.3.5 (v3.3.5:62cf4e77f785, Mar  9 2014, 10:37:12) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('helloworld')
+helloworld
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> 
