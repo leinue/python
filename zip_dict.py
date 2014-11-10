58f887a5b3d8bae3cdcd0848f7c1299eed2b6d2c
ivy <597055914@qq.com>
2014-11-10 13:33:28 +0800

    continue

---
+++ zip_dict.py
@@ -0,0 +1,9 @@
+Python 3.3.5 (v3.3.5:62cf4e77f785, Mar  9 2014, 10:37:12) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> key=(1,2,3)
+>>> value=('a','b','c')
+>>> zip(key,value)
+<zip object at 0x02425DC8>
+>>> dict(zip(key,value))
+{1: 'a', 2: 'b', 3: 'c'}
+>>> 
