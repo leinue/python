2041bf0811cfaae54e7287131bfd2629f0347c0a
ivy <597055914@qq.com>
2014-11-17 21:41:59 +0800

    update

--- 按元素值递增排序.py
+++ 按元素值递增排序.py
@@ -6,10 +6,14 @@ def bubble(bubbleList):
     listLength=len(bubbleList)
     while listLength>0:
         for i in range(listLength-1):
+            print(str(i)+"::"+str(bubbleList[i])+':'+str(bubbleList[i+1]))
             if bubbleList[i]>bubbleList[i+1]:
                 bubbleList[i]=bubbleList[i]+bubbleList[i+1]
                 bubbleList[i+1]=bubbleList[i]-bubbleList[i+1]
                 bubbleList[i]=bubbleList[i]-bubbleList[i+1]
+            print('******')
+            print(str(i)+"::"+str(bubbleList[i])+':'+str(bubbleList[i+1]))
+        print('------------')
         listLength-=1
     print(bubbleList)
     
