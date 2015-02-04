ddbd415c59f85c43d2065f8fdb04dd67007372a9
ivy <597055914@qq.com>
2015-02-04 16:46:03 +0800

    update

--- pythonChallenge2.py
+++ pythonChallenge2.py
@@ -1,5 +1,11 @@
 import string
+import re
 
 a="map"
 b=a.maketrans('abcdefghijklmnopqrstuvwxyz','cdefghijklmnopqrstuvwxyzab')
 print (a.translate(b))
+
+a=open('a.txt','r')
+c=a.read()
+b=re.findall(r'[a-zA-Z]',c)
+print(''.join(b))
